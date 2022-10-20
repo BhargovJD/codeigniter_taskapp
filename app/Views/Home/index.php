113543183be019fd413527553c48a98f7232362d
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2022-10-20 12:59:45 +0530

    2 View and Controller

---
+++ app/Views/Home/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend("layouts/defaults") ?>
+
+<?= $this->section("title") ?>
+Home
+<?= $this->endSection()?>
+
+
+<?= $this->section("content") ?>
+<h1>Home Content</h1>
+<?= $this->endSection()?>
---
+++ app/Views/Tasks/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend("layouts/defaults") ?>
+
+<?= $this->section("title") ?>
+Task
+<?= $this->endSection()?>
+
+
+<?= $this->section("content") ?>
+<h1>Task Content</h1>
+<?= $this->endSection()?>
