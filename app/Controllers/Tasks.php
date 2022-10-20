926c98a71be88f14aec35061cc260b7df8468797
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2022-10-20 13:13:20 +0530

    Passing data from controller to view

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -6,6 +6,10 @@ class Tasks extends BaseController
 {
     public function index()
     {
-        return view("Tasks/index.php");
+        $data = [
+            ['id'=>1,'desc'=>'First task'],
+            ['id'=>2,'desc'=>'Second task']
+        ];
+        return view("Tasks/index.php",['tasks'=>$data]);
     }
 }
--- app/Views/Tasks/index.php
+++ app/Views/Tasks/index.php
@@ -7,4 +7,12 @@ Task
 
 <?= $this->section("content") ?>
 <h1>Task Content</h1>
+<ul>
+    <?php foreach($tasks as $task): ?>
+        <li>
+            <?= $task['id'] ?>
+            <?= $task['desc'] ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
 <?= $this->endSection()?>
